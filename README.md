f46e60aeb0730a9de3e716ce58e12ef410e1a200
Dylan Morrison <dylanjmorrison30@gmail.com>
2021-12-06 12:11:29 +0000

    Readme script

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Advent of Code 2021
 
-![](https://img.shields.io/badge/day%20📅-4-blue)
-![](https://img.shields.io/badge/stars%20⭐-8-yellow)
-![](https://img.shields.io/badge/days%20completed-3-red)
+![](https://img.shields.io/badge/day%20📅-6-blue)
+![](https://img.shields.io/badge/stars%20⭐-12-yellow)
+![](https://img.shields.io/badge/days%20completed-6-red)
 
 Trying to solve https://adventofcode.com/2021
--- updateReadMe.py
+++ updateReadMe.py
@@ -1,4 +1,18 @@
 import re
 
-with open(file="README.md", mode="rw"):
-    ``
+lines = open("README.md", mode="r").readlines()
+
+
+pattern = r"-(\d{1,2})-"
+
+curr_day = int(re.findall(pattern ,str(lines[2]))[0])
+next_day = "-" + str(curr_day+1) + "-"
+total_stars =  "-" + str((curr_day+1) * 2) + "-"
+
+lines[2] = re.sub(pattern, next_day ,lines[2])
+lines[3] = re.sub(pattern, total_stars ,lines[3])
+lines[4] = re.sub(pattern, next_day ,lines[4])
+
+open("README.md", mode="w").write("".join(lines))
+
+print("README Updated")
